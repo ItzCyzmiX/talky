b36bad91e9a7978bf20282721fd3b300fd2cd5af
CyzmiX <itzmedigamingx@gmail.com>
2026-09-11 19:27:36 +0100

    restart bug

--- main.py
+++ main.py
@@ -8,18 +8,4 @@ from bot.bot import run_bot
 
 logging.basicConfig(level=logging.INFO)
 
-
-def restart_bot():
-    while True:
-        cmd = input()
-        match cmd.strip().lower():
-            case "r":
-                logging.info("Restarting...")
-                os.execv(sys.executable, ["python"] + sys.argv)
-            case _:
-                pass
-
-
-threading.Thread(target=restart_bot, daemon=True).start()
-
-runner = asyncio.run(run_bot())
+asyncio.run(run_bot())
